c0d86e030411d02564a5d1e2ce8edfa2a5dbaf99
Sean <Seanamccormick7@gmail.com>
2025-10-25 14:48:10 -0700

    feat(components): add reusable Navbar component
    - Create Navbar with Next.js Link navigation
    - Implement responsive navigation with CSS modules

---
+++ bootcamp-milestone-2/src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Link from "next/link";
+import style from "./navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <header>
+      <nav className={style.navbar}>
+        <h1 className={style.logo}>
+          <Link href="/">personal website</Link>
+        </h1>
+        <ul className={style.navList}>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/portfolio">Portfolio</Link>
+          </li>
+          <li>
+            <Link href="/resume">Resume</Link>
+          </li>
+          <li>
+            <Link href="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
